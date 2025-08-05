92bd3dfcd5bef39e8f66ec57d9e3dcc65203fc37
AMIR <135604409+HESL0@users.noreply.github.com>
2025-08-05 10:33:14 +0330

    Update listStore.js

---
+++ src/store/listStore.js
@@ -0,0 +1,25 @@
+import { defineStore } from 'pinia'
+import { ref } from 'vue'
+import { useBoardStore } from './boardStore'
+
+export const useListStore = defineStore('list', () => {
+  const boardStore = useBoardStore()
+
+  function addList(boardId, title) {
+    const board = boardStore.boards.find(b => b.id === boardId)
+    if (board) board.lists.push({ id: Date.now(), title, cards: [] })
+  }
+  function editList(boardId, listId, title) {
+    const board = boardStore.boards.find(b => b.id === boardId)
+    if (!board) return
+    const list = board.lists.find(l => l.id === listId)
+    if (list) list.title = title
+  }
+  function deleteList(boardId, listId) {
+    const board = boardStore.boards.find(b => b.id === boardId)
+    if (!board) return
+    board.lists = board.lists.filter(l => l.id !== listId)
+  }
+
+  return { addList, editList, deleteList }
+})
